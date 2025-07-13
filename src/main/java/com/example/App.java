832ccb01ed97254f671e03857fa287106fdea7dd
pawan6673 <pawanranjan6673@gmail.com>
2025-07-14 03:10:00 +0530

    done

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,9 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Modified message - triggering CI!");
+>>>>>> 9c30158 (Updated message)
     }
 }
+
